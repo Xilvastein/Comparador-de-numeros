bff3e5704484fc11eb9fa1d34d6be54c7d16f830
Xilva <kirfrf@hotmail.com>
2022-06-19 21:34:59 -0300

    first commit

---
+++ Comparando numero038.py
@@ -0,0 +1,9 @@
+num1 = int (input(' Digite o Primeiro número:'))
+num2 = int(input('Digite o Segundo número:'))
+if num1 > num2:
+    print('O primeiro é MAIOR.')
+elif num2 > num1:
+    print('O segundo é MAIOR.')
+else:
+    num1 == num2
+    print('Não existe valor maior, os dois são iguais.')
